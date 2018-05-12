dc3f63c79849bcba9ee4feb27703d2de0522ab5a
Thoughtscript <adam.gerard@gmail.com>
2018-05-12 23:08:01 +0100

    2.4 - Corrected Redux to reflect changed 1.9

--- 2.4_as_the_crow_flies_redux/com/thinkful/drill/AsTheCrowFliesReduxSolution.java
+++ 2.4_as_the_crow_flies_redux/com/thinkful/drill/AsTheCrowFliesReduxSolution.java
@@ -6,7 +6,6 @@ public class AsTheCrowFliesReduxSolution {
 
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
-
     String oneMoreTime = "y";
 
     do {
@@ -23,20 +22,17 @@ public class AsTheCrowFliesReduxSolution {
       System.out.println("Please enter second y coordinate in degrees: ");
       double yTwo = scanner.nextDouble();
 
-      /**
-       * Equation time! Here we go:
-       */
-
       double deltaY = Math.toRadians(yTwo - yOne);
       xOne = Math.toRadians(xOne);
       xTwo = Math.toRadians(xTwo);
-      double a = Math.pow(Math.sin((xTwo - xOne)/2), 2) + Math.cos(xOne) * Math.cos(xTwo) * Math.pow(Math.sin(deltaY / 2), 2);
-      double c  = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+      double a = Math.pow(Math.sin((xTwo - xOne) / 2), 2) + Math.cos(xOne) * Math.cos(xTwo) * Math
+          .pow(Math.sin(deltaY / 2), 2);
+      double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
       double d = 6371 * c;
 
-      System.out.printf("The calculated distance is %f km", d);
+      System.out.printf("The calculated distance is %f km \n", d);
 
-      String result = "Whoa! Too far!";
+      String result = "Whoa! Too far! \n";
 
       if (d < 1.00) {
         result = "Dude! We are practically in the same place! \n";
