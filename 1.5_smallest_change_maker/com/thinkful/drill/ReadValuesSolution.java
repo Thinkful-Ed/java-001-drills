3b1c91ce07af0362b7aca2bde0bdb980ba036fae
Vincent Ramdhanie <vramdhanie@gmail.com>
2018-06-01 21:20:20 -0400

    Update drill 1.5 to use simpler solution

--- 1.5_smallest_change_maker/com/thinkful/drill/ReadValuesSolution.java
+++ 1.5_smallest_change_maker/com/thinkful/drill/ReadValuesSolution.java
@@ -8,8 +8,11 @@ public class ReadValuesSolution {
     Scanner scanner = new Scanner(System.in);
 
     System.out.println("Please enter $ amount: ");
-    Double amount = scanner.nextDouble() * 100;
-    Integer amountInt = amount.intValue();
+
+    // read the input from the user in dollars and cents
+    //and multiply by 100 to convert to cents
+    double amount = scanner.nextDouble();
+    int amountInt = (int)Math.floor(amount * 100);
 
     int hundreds = 0;
     int fifties = 0;
@@ -22,64 +25,55 @@ public class ReadValuesSolution {
     int nickles = 0;
     int pennies = 0;
 
-    if (amountInt > 10000) {
-      int rem = amountInt % 10000;
-      hundreds = (amountInt - rem) / 10000;
-      amountInt = rem;
-    }
+    // calculate number of $100 bills
+    hundreds = amountInt / 10000;
+    amountInt = amountInt % 100000;
 
-    if (amountInt > 5000) {
-      int rem = amountInt % 5000;
-      fifties = (amountInt - rem) / 5000;
-      amountInt = rem;
-    }
+    // calculate number of $50 bills
+    fifties = amountInt / 5000;
+    amountInt = amountInt % 5000;
 
-    if (amountInt > 2000) {
-      int rem = amountInt % 2000;
-      twenties = (amountInt - rem) / 2000;
-      amountInt = rem;
-    }
+    // calculate number of $20 bills
+    twenties = amountInt / 2000;
+    amountInt = amountInt % 2000;
 
-    if (amountInt > 1000) {
-      int rem = amountInt % 1000;
-      tens = (amountInt - rem) / 1000;
-      amountInt = rem;
-    }
+    // calculate number of $10 bills
+    tens = amountInt / 1000;
+    amountInt = amountInt % 1000;
 
-    if (amountInt > 500) {
-      int rem = amountInt % 500;
-      fives = (amountInt - rem) / 500;
-      amountInt = rem;
-    }
+    // calculate number of $5 bills
+    fives = amountInt / 500;
+    amountInt = amountInt % 500;
 
-    if (amountInt > 100) {
-      int rem = amountInt % 100;
-      ones = (amountInt - rem) / 100;
-      amountInt = rem;
-    }
+    // calculate the number of $1 bills
+    ones = amountInt / 100;
+    amountInt = amountInt % 100;
 
-    if (amountInt > 25) {
-      int rem = amountInt % 25;
-      quarters = (amountInt - rem) / 25;
-      amountInt = rem;
-    }
+    // calculate the number of quarters
+    quarters = amountInt / 25;
+    amountInt = amountInt % 25;
 
-    if (amountInt > 10) {
-      int rem = amountInt % 10;
-      dimes = (amountInt - rem) / 10;
-      amountInt = rem;
-    }
+    // calculate the number of dimes
+    dimes = amountInt / 10;
+    amountInt = amountInt % 10;
 
-    if (amountInt > 5) {
-      int rem = amountInt % 5;
-      nickles = amountInt - rem / 5;
-      amountInt = rem;
-    }
+    // calculate the number of nickles and pennies
+    nickles = amountInt / 5;
+    pennies = amountInt % 5;
 
-    pennies = amountInt;
 
     System.out.printf(
-        "You will receive %d $100 Bills, %d $50 Bills, %d $20 Bills, %d $10 Bills, %d $5 Bills, %d $1 Bills, %d Quarters, %d Dimes, %d Nickles, %d Pennies",
-        hundreds, fifties, twenties, tens, fives, ones, quarters, dimes, nickles, pennies);
+        "To make $%.2f, you will receive\n"
+        + "%d $100 Bills\n"
+        + "%d $50 Bills\n"
+        + "%d $20 Bills\n"
+        + "%d $10 Bills\n"
+        + "%d $5 Bills\n"
+        + "%d $1 Bills\n"
+        + "%d Quarters\n"
+        + "%d Dimes\n"
+        + "%d Nickles\n"
+        + "%d Pennies\n",
+        amount, hundreds, fifties, twenties, tens, fives, ones, quarters, dimes, nickles, pennies);
   }
 }
